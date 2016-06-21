e84d7c714f08b09ac27b29d929d1ffe54d039751
Tyly04 <tyly04@gmail.com>
2016-06-20 17:00:47 -0700

    Added more to example and tutorial
    added a tutorial, fixed some bugs, and added more to the tutorial.

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ var TAS = {
     for(i = 0; i < TAS.rooms.length; i++){
       if(TAS.rooms[i].id === id){
         console.log("TAS: Room Found!");
-        recentRoom = TAS.rooms[i];
         TAS.showRoom(TAS.rooms[i]);
       } else {
         console.warn("TAS: Room Not found!");
@@ -26,29 +25,30 @@ var TAS = {
   },
   showRoom: function(room){
     document.body.innerHTML = room.displayText + " The room contains " + room.inventory + ".";
-    TAS.gotoRoom(room);
     console.log("TAS: Showing room description...");
     TAS.takeItems(room);
+    TAS.gotoRoom(room);
   },
   gotoRoom: function(room){
     for (i = 0; i < room.accessibleRoomsId.length; i++){
       console.log("TAS: Finding available rooms...");
+      document.body.innerHTML += "<p>Here is a list of available rooms that you can go to:</p>";
       document.body.innerHTML = document.body.innerHTML + "<p><button onClick = \"TAS.roomReader(" + room.accessibleRoomsId[i] +")\">" + room.placesName[i] +"</button></p>";
     }
   },
   //Warning! INVENTORY SYSTEM IS NOT COMPLETE. DO NOT USE.
   takeItems: function(room){
-    var thisRoom = recentRoom;
     document.body.innerHTML = document.body.innerHTML + "<p>What items would you like to take?</p>";
     for(i = 0; i < room.inventory.length; i++){
       console.log("TAS: Finding inventory items...");
-      document.body.innerHTML = document.body.innerHTML + " <p><button onClick = \"TAS.inventoryAdd(" + i + "])\">" + room.inventory[i] + "</button></p>";
+      document.body.innerHTML = document.body.innerHTML + " <p><button onClick = \"TAS.inventoryAdd(" + room + "," + i + "])\">" + room.inventory[i] + "</button></p>";
     }
   },
-  inventoryAdd: function(roomInventoryItemId){
-    recentRoom.inventory.splice(recentRoom.inventory[roomInventoryItemId])
-    TAS.inventory.push("" + recentRoom.inventory[roomInventoryItemId] + "");
-    console.log("TAS:" + recentRoom.inventory[roomInventoryItemId] + " successfully added.");
+  inventoryAdd: function(room, roomInventoryItemId){
+    console.log("TAS: Attempting to add item id " + roomInventoryItemId + " from " + room)
+    room.inventory.splice(room.inventory[roomInventoryItemId])
+    TAS.inventory.push("" + room.inventory[roomInventoryItemId] + "");
+    console.log("TAS:" + room.inventory[roomInventoryItemId] + " successfully added.");
   }
 
 }
--- readme.md
+++ readme.md
@@ -4,3 +4,25 @@ What is TJTBAS? It stands for Tyly's Java-Script Table Based Adventure System. T
 ``` html
 <script src = "main.js"></script>
 ```
+# How to use TJTBAS
+First, create an html file, and add everything you need to. Link using this code:
+``` html
+<script src = "main.js"></script>
+```
+The code won't work as soon as the document loads, so you'll have to create a function which holds the starting code.
+Once that's done, let's create our first room. First of all, everything having to do with TJTBAS is stored under the TAS object. In order to create our room, we're going to have to create a new variable:
+```html
+<script>
+function start(){
+var room1 = new TAS.room("Name your room here.", 1, "Your room Id is the one in the middle, and it is what is used to identify your room. This is the description of the room.");
+}
+</script>
+<body>
+  <button onClick = "start()">Click here to start</button>
+</body>
+```
+We've created our first room! It has a name, an id, and a description. We can now simply load the room using the following command:
+``` javascript
+TAS.roomReader(1);
+```
+In between the brackets, put your room id, and TJTBAS will load the room, and show the description.
